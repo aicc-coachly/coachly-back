e6ddf01578892bbe74c1f178d81f964801ad0a59
geonnim <yy4338@gmail.com>
2024-11-14 11:57:52 +0900

    chore/Prepare project for production deployment

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,12 @@
 # Node.js 베이스 이미지 사용
 FROM node:20.15.1
 
-# Python3 설치
-RUN apt-get update && \
-    apt-get install -y python3 python3-pip && \
-    rm -rf /var/lib/apt/lists/*
-
 # 작업 디렉토리 설정
 WORKDIR /opt/app
 
 # 의존성 파일 복사 및 설치
 COPY package*.json ./
 RUN npm install
-
-# Python 의존성 설치
-COPY requirements.txt .
-RUN pip3 install -r requirements.txt
 
 # 소스 파일 복사
 COPY . .
--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const socketIo = require("socket.io");
-const pool = require("./database/database"); // PostgreSQL 연결 풀 가져오기
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const socketIo = require('socket.io');
+const pool = require('./database/database'); // PostgreSQL 연결 풀 가져오기
+require('dotenv').config();
 
 const PORT = 8000;
 const app = express();
-const server = app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Server is running on ${PORT}`)
+);
 
 // 전역 캐시 객체 정의
 const cache = {};
 
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["my-custom-header", "Content-Type"],
+    origin: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header', 'Content-Type'],
     credentials: true,
   },
 });
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use(require("./routes/authRoutes"));
-app.use(require("./routes/chatRoutes"));
-app.use(require("./routes/trainerRoutes"));
-app.use(require("./routes/userRoutes"));
-app.use(require("./routes/authTokenRoutes"));
-app.use(require("./routes/refundRoutes"));
-app.use(require("./routes/scheduleRoutes"));
-app.use(require("./routes/paymentsRoutes"));
+app.use(require('./routes/authRoutes'));
+app.use(require('./routes/chatRoutes'));
+app.use(require('./routes/trainerRoutes'));
+app.use(require('./routes/userRoutes'));
+app.use(require('./routes/authTokenRoutes'));
+app.use(require('./routes/refundRoutes'));
+app.use(require('./routes/scheduleRoutes'));
+app.use(require('./routes/paymentsRoutes'));
 
 // 메시지 저장을 위한 함수 (데이터베이스에 연결)
 async function saveMessagesToDatabase(roomId) {
@@ -42,56 +44,62 @@ async function saveMessagesToDatabase(roomId) {
 
   const client = await pool.connect();
   try {
-    await client.query("BEGIN");
-    const query = "INSERT INTO chat_message (room_id, sender_name, content) VALUES ($1, $2, $3)";
+    await client.query('BEGIN');
+    const query =
+      'INSERT INTO chat_message (room_id, sender_name, content) VALUES ($1, $2, $3)';
     for (const message of messages) {
-      console.log(`Saving message - Room ID: ${roomId}, Sender Name: ${message.sender_name}, Content: ${message.content}`);
+      console.log(
+        `Saving message - Room ID: ${roomId}, Sender Name: ${message.sender_name}, Content: ${message.content}`
+      );
       await client.query(query, [roomId, message.sender_name, message.content]);
     }
-    await client.query("COMMIT");
+    await client.query('COMMIT');
     cache[roomId] = []; // 캐시 초기화
     console.log(`Messages for room ${roomId} saved to database.`);
   } catch (error) {
-    await client.query("ROLLBACK");
+    await client.query('ROLLBACK');
     console.error(`Error saving messages for room ${roomId}:`, error);
   } finally {
     client.release();
   }
 }
 
-io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
 
-  socket.on("joinRoom", (roomId) => {
+  socket.on('joinRoom', (roomId) => {
     if (!socket.rooms.has(roomId)) {
       socket.join(roomId);
       console.log(`User joined room: ${roomId}`);
     }
   });
 
-  socket.on("sendMessage", (message) => {
-    console.log("Received message:", message); // 메시지 로깅 추가
+  socket.on('sendMessage', (message) => {
+    console.log('Received message:', message); // 메시지 로깅 추가
     const { roomId, senderName, content, timestamp } = message;
 
     if (!cache[roomId]) cache[roomId] = [];
     const isDuplicate = cache[roomId].some(
-      (msg) => msg.content === content && msg.senderName === senderName && msg.timestamp === timestamp
+      (msg) =>
+        msg.content === content &&
+        msg.senderName === senderName &&
+        msg.timestamp === timestamp
     );
 
     if (!isDuplicate) {
       cache[roomId].push(message);
-      io.to(roomId).emit("messageReceived", message);
+      io.to(roomId).emit('messageReceived', message);
     }
   });
 
-  socket.on("leaveRoom", (roomId) => {
+  socket.on('leaveRoom', (roomId) => {
     socket.leave(roomId);
     console.log(`User left room: ${roomId}`);
     saveMessagesToDatabase(roomId);
   });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
     const rooms = Array.from(socket.rooms).filter((room) => room !== socket.id);
     rooms.forEach((roomId) => saveMessagesToDatabase(roomId));
   });
